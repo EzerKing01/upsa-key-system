f982df5d7f7390acbf934a8648267d0670d6bf83
EzerKing01 <ezernorms@gmail.com>
2026-05-06 15:13:37 +0000

    Fix api/ route 404; add test route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,12 +7,8 @@ use App\Http\Controllers\KeyListController;
 use App\Http\Controllers\KeyController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/clear-config', function() {
-    \Artisan::call('config:clear');
-    \Artisan::call('cache:clear');
-    \Artisan::call('route:clear');
-    return 'Cache cleared. Now remove this route.';
+Route::get('/test', function () {
+    return 'API test works';
 });
 /*
 |--------------------------------------------------------------------------
