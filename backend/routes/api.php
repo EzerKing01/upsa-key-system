e2efeceb468b2632825c7a7103976f13628ab722
EzerKing01 <ezernorms@gmail.com>
2026-05-06 14:39:29 +0000

    Add temporary /clear-config route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,6 +7,13 @@ use App\Http\Controllers\KeyListController;
 use App\Http\Controllers\KeyController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/clear-config', function() {
+    \Artisan::call('config:clear');
+    \Artisan::call('cache:clear');
+    \Artisan::call('route:clear');
+    return 'Cache cleared. Now remove this route.';
+});
 /*
 |--------------------------------------------------------------------------
 | API Routes
